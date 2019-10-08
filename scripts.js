f2aefcb23ce82cfd70b6d2230bfeb667f4596de7
= <=>
2019-10-08 21:47:47 +0700

    add local storage

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-let thingsTodo = [];
+let thingsTodo = JSON.parse(localStorage.getItem('data'));
+renderTodos();
 let theClone = thingsTodo;
 function addThingToDo() {
     let thingTodo = { text: "", isDone: false };
     thingTodo.text = document.getElementById('input').value;
     thingsTodo.push(thingTodo);
     renderTodos();
+    setItem();
 }
 function renderTodos() {
+    if (thingsTodo === null) { thingsTodo = []; }
     let listHTML = thingsTodo.map((item, i) => {
-        let items = 
-        `<li class="${item.isDone ? 'done' : 'not-done'}">${item.text} 
+        let items =
+            `<li class="${item.isDone ? 'done' : 'not-done'}">${item.text} 
             <a href="#" onclick="removeItem(${i})">x</a> 
             <a href="#" onclick="toggleDone(${i})">${item.isDone ? 'Toggle not Done' : 'Toggle Done'}</a>
         </li>`;
@@ -20,27 +23,29 @@ function renderTodos() {
 function removeItem(i) {
     thingsTodo.splice(i, 1);
     renderTodos(thingsTodo);
+    setItem();
 }
 function toggleDone(i) {
     thingsTodo[i].isDone = !thingsTodo[i].isDone;
     renderTodos()
     console.log(thingsTodo[i].isDone)
+    setItem();
 }
-function showThingsDone(){
-    thingsTodo = thingsTodo.filter((thing)=>{
-        if (thing.isDone === true){return true}
+function showThingsDone() {
+    thingsTodo = thingsTodo.filter((thing) => {
+        if (thing.isDone === true) { return true }
     })
     renderTodos();
     thingsTodo = theClone;
 }
-function showThingsUndone(){
-    thingsTodo = thingsTodo.filter((thing)=>{
-        if (thing.isDone === false){return true}
+function showThingsUndone() {
+    thingsTodo = thingsTodo.filter((thing) => {
+        if (thing.isDone === false) { return true }
     })
     renderTodos();
     thingsTodo = theClone;
 }
-function showAll(){
+function showAll() {
     thingsTodo = theClone;
     renderTodos();
 }
@@ -48,3 +53,7 @@ function showAll(){
 document.getElementById('addButton').addEventListener('click', function () {
     addThingToDo();
 })
+//----local storage--------
+function setItem() {
+    localStorage.setItem("data", JSON.stringify(thingsTodo));
+}
