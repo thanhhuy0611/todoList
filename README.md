c725e3f3c8a1df6d7037ceddb198b844bea038c1
= <=>
2019-10-08 17:04:37 +0700

    submit firsttime

---
+++ README.md
@@ -0,0 +1,30 @@
+# CoderSchool FTW - Todo List
+
+Created with love by: Huy Tran
+
+### View online at [here](https://vibrant-babbage-ccb239.netlify.com/)
+
+
+The following **required** functionalities are completed and implemented:
+- [x] Update Todo List on Page
+- [x] Add a remove button
+- [x] Toggle between done and not done
+- [x] Strikethrough
+- [x] Add a checkbox filter to show only done or undone
+- [ ] Persist locally
+
+## License
+
+    Copyright [2019] [Tran Thanh Huy]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ scripts.js
@@ -0,0 +1,50 @@
+let thingsTodo = [];
+let theClone = thingsTodo;
+function addThingToDo() {
+    let thingTodo = { text: "", isDone: false };
+    thingTodo.text = document.getElementById('input').value;
+    thingsTodo.push(thingTodo);
+    renderTodos();
+}
+function renderTodos() {
+    let listHTML = thingsTodo.map((item, i) => {
+        let items = 
+        `<li class="${item.isDone ? 'done' : 'not-done'}">${item.text} 
+            <a href="#" onclick="removeItem(${i})">x</a> 
+            <a href="#" onclick="toggleDone(${i})">${item.isDone ? 'Toggle not Done' : 'Toggle Done'}</a>
+        </li>`;
+        return items;
+    }).join("")
+    document.getElementById('listThingsToDo').innerHTML = listHTML;
+}
+function removeItem(i) {
+    thingsTodo.splice(i, 1);
+    renderTodos(thingsTodo);
+}
+function toggleDone(i) {
+    thingsTodo[i].isDone = !thingsTodo[i].isDone;
+    renderTodos()
+    console.log(thingsTodo[i].isDone)
+}
+function showThingsDone(){
+    thingsTodo = thingsTodo.filter((thing)=>{
+        if (thing.isDone === true){return true}
+    })
+    renderTodos();
+    thingsTodo = theClone;
+}
+function showThingsUndone(){
+    thingsTodo = thingsTodo.filter((thing)=>{
+        if (thing.isDone === false){return true}
+    })
+    renderTodos();
+    thingsTodo = theClone;
+}
+function showAll(){
+    thingsTodo = theClone;
+    renderTodos();
+}
+//------Catch Event----------
+document.getElementById('addButton').addEventListener('click', function () {
+    addThingToDo();
+})
